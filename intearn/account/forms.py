3515459517ed3c3913a83c86209f8609ba754808
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-10-02 22:55:49 +0530

    Fixed bugs in backend in accounts.forms

--- intearn/account/forms.py
+++ intearn/account/forms.py
@@ -19,9 +19,9 @@ class UserCreationForm(forms.ModelForm):
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+        if password1 and password2 and password1 == password2:
+            return password2
+        raise forms.ValidationError("Passwords don't match")
 
     def save(self, commit=True):
         user = super().save(commit=False)
